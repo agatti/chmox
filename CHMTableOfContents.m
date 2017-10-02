ce3cc8c0e9cfd2fd8ab390329dd5ff9811e5305b
Alessandro Gatti <a.gatti@frob.it>
2017-10-02 13:19:01 +0800

    Fix UTF-8 encoding in source files.

--- CHMTableOfContents.m
+++ CHMTableOfContents.m
@@ -1,6 +1,6 @@
 //
 // Chmox a CHM file viewer for Mac OS X
-// Copyright (c) 2004 Stéphane Boisson.
+// Copyright (c) 2004 St√©phane Boisson.
 // Copyright (c) 2017 Alessandro Gatti.
 //
 // Chmox is free software; you can redistribute it and/or modify it
--- main.m
+++ main.m
@@ -18,6 +18,4 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 
-@import Cocoa;
-
 int main(int argc, const char *argv[]) { return NSApplicationMain(argc, argv); }
